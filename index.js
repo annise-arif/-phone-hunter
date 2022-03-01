a91098386700c520dcc709b9c53a7de4cc248065
Annise Arif <83833939+annise-arif@users.noreply.github.com>
2022-03-01 13:51:46 +0600

    added display result

--- index.js
+++ index.js
@@ -7,9 +7,25 @@ const searchPhone = () => {
     
     fetch(url)
     .then(res => res.json())
-    .then(res => displayResult(res));
+    .then(res => displayResult(res.data));
 }
 const displayResult = res => {
-    console.log(res.data);
-    
+    const searchResult = document.getElementById('search-result');
+    res.forEach(element => {
+        console.log(element);
+        const div = document.createElement('div');
+        div.classList.add('cal');
+        div.innerHTML = `
+        <div class="card h-100">
+           <img src="${element.image}" class="card-img-top" alt="...">
+           <div class="card-body">
+              <h5 class="card-title">${element.phone_name}</h5>
+              <h6 class="card-title">${element.slug}</h6>
+              <p class="card-text">${element.brand}</p>
+           </div>
+         </div>
+        `;
+        searchResult.appendChild(div);
+    })
+
 }
