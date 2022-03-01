c8ded87a4c4996f1aa80d7360e72a234c6ddf8f1
Annise Arif <83833939+annise-arif@users.noreply.github.com>
2022-03-01 11:54:31 +0600

    added js

---
+++ index.js
@@ -0,0 +1,6 @@
+document.getElementById('search-btn').addEventListener('click', () => {
+    const searchField = document.getElementById('input-field');
+    const searchValue = searchField.value;
+    console.log(searchValue);
+
+})
