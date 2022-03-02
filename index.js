8284c4961fa47ff50ad2ecc1ea7b81e5419aa62e
Annise Arif <83833939+annise-arif@users.noreply.github.com>
2022-03-02 11:27:57 +0600

    added some arror handling

--- index.js
+++ index.js
@@ -3,16 +3,23 @@ const searchPhone = () => {
     const searchText = searchField.value;
     searchField.value = '';
 
-    const url = `https://openapi.programming-hero.com/api/phones?search=${searchText}`
+    if(searchText == ''){
+        window.alert("Please write a phone name");
+    }
+    else{
+        const url = `https://openapi.programming-hero.com/api/phones?search=${searchText}`;
+        fetch(url)
+          .then(res => res.json())
+          .then(res => displayResult(res.data));
+    }
     
-    fetch(url)
-    .then(res => res.json())
-    .then(res => displayResult(res.data));
+    
 }
 const displayResult = res => {
     const searchResult = document.getElementById('search-result');
+    searchResult.textContent = '';
     res.forEach(element => {
-        console.log(element);
+        // console.log(element);
         const div = document.createElement('div');
         div.classList.add('cal');
         div.innerHTML = `
@@ -32,15 +39,36 @@ const displayResult = res => {
 
 const loadDetail = slug => {
     // console.log(slug);
+    
     const url = `https://openapi.programming-hero.com/api/phone/${slug}`;
     fetch(url)
-    .then(res => res.json())
-    .then(res => displayDetail(res));
+      .then(res => res.json())
+      .then(res => displayDetail(res));
     
 
 }
 
 const displayDetail = (res) => {
-    console.log(res.data.mainFeatures)
+    // console.log(res.data.mainFeatures)
+    const detailField = document.getElementById('product-detail');
+    detailField.textContent = '';
+    const div = document.createElement('div');
+    div.classList.add('col');
+    div.innerHTML = `
+    <div class="card mx-auto p-3 mb-5" style="width: 40rem;">
+       <img src="${res.data.image}" class="card-img-top" alt="...">
+       <div class="card-body">
+       <h3 class="card-title">${res.data.name}</h3>
+       <p class="card-text"><b>Brand : </b>${res.data.brand}</p>
+       <p class="card-text"><b>First release : </b>${res.data.releaseDate}</p>
+       <p class="card-text"><b>Storage : </b>${res.data.mainFeatures.storage}</p>
+       <p class="card-text"><b>Display size : </b>${res.data.mainFeatures.displaySize}</p>
+       <p class="card-text"><b>Chip set : </b>${res.data.mainFeatures.chipSet}</p>
+       <p class="card-text"><b>Memory : </b>${res.data.mainFeatures.memory}</p>
+       <p class="card-text"><b>Sensors : </b>${res.data.mainFeatures.sensors}</p>
+       </div>
+    </div>
+    `;
+    detailField.appendChild(div);
    
 }
